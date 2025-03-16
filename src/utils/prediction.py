ca5ec705f092e71311c4100ef66ab704fefc3f91
agnesrachel <wtyueng@yahoo.com.hk>
2025-03-16 17:47:11 +0800

    create prediction function

--- src/utils/prediction.py
+++ src/utils/prediction.py
@@ -8,7 +8,6 @@ import pandas as pd
 import numpy as np
 
 def sign_accuracy(y_true, y_pred):
-
     y_true_diff = y_true[:, -1] - y_true[:, 0]
     y_pred_diff = y_pred[:, -1] - y_pred[:, 0]
 
@@ -22,24 +21,33 @@ def sign_accuracy(y_true, y_pred):
 def preparing_prediction_data(ticker_symbol):
         raw_data = data_collection.get_stock_data( ticker_symbol = ticker_symbol, period='6mo')
         raw_data = raw_data.tail(25)
-        min_val = raw_data.min()  # Min over all features
-        max_val = raw_data.max()  # Max over all features
+        min_val = raw_data.min()  
+        max_val = raw_data.max()  
         data_scaled = ( ((raw_data - min_val))/ (max_val - min_val) ) 
         model_predict_data = np.array(data_scaled)
-        return raw_data,model_predict_data,max_val,min_val
+        return raw_data,model_predict_data[np.newaxis,:],max_val,min_val
+
+def prediction(ticker_symbol):
+    raw_data,model_predict_data,max_val, min_val = preparing_prediction_data(ticker_symbol)
+    temp_model_path = f"../../models/{ticker_symbol}.h5"
+    model = load_model(temp_model_path, custom_objects={'sign_accuracy': sign_accuracy})
+    predicted_prices = model.predict(model_predict_data)
+    predicted_prices_normal = np.array(((predicted_prices) * (max_val["close"] - min_val["close"]) + min_val["close"])[0])
+    percentage_change = (predicted_prices_normal[4] - raw_data["close"].tail(1))/raw_data["close"].tail(1)
+    percentage_change =  percentage_change.iloc[0] * 100.0
+    return predicted_prices_normal,percentage_change
 
 
 
 if __name__ == '__main__':
-    raw_data,model_predict_data, max_val, min_val = preparing_prediction_data("^HSI")
-    print(model_predict_data)
-    temp_model_path = "../../models/^HSI.h5"
-    model = load_model(temp_model_path, custom_objects={'sign_accuracy': sign_accuracy})
-    predicted_prices = model.predict(model_predict_data[np.newaxis,:])
-    predicted_prices_normal = np.array(((predicted_prices) * (max_val["close"] - min_val["close"]) + min_val["close"])[0])
-    percentage_change = (predicted_prices_normal[4] - raw_data["close"].tail(1))/raw_data["close"].tail(1)
-    percentage_change =  percentage_change.iloc[0] * 100.0
+    ticker_symbol = "^HSI"
+    predicted_prices,percentage_change = prediction(ticker_symbol)
+    print(predicted_prices)
     print(percentage_change)
+
+
+   
+  
 
     
 
