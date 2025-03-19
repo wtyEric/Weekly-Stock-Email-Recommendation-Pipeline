47ac359b8f1dcf2f6b4985e978de0674e384dc38
agnesrachel <wtyueng@yahoo.com.hk>
2025-03-19 14:06:30 +0800

    Fix Bug

--- stock prediction API/app.py
+++ stock prediction API/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 import os, sys
+import numpy as np
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 from utils.recommendation import get_all_predictions
 
@@ -9,6 +10,13 @@ app = Flask(__name__)
 def predictions():
     try:
         predictions = get_all_predictions()
+        # Convert NumPy arrays to Python lists
+        for stock in predictions.values():
+            if isinstance(stock['prediction'], np.ndarray):
+                stock['prediction'] = stock['prediction'].tolist()
+            if isinstance(stock['percentage_change'], np.float32):
+                stock['percentage_change'] = float(stock['percentage_change'])
+                
         return jsonify({
             'success': True,
             'data': predictions
